5fd53c5e0661a4594d6b3b0be99c1944ebe8d769
Purva Saxena <71626792+purva-saxena@users.noreply.github.com>
2021-06-05 01:54:07 +0530

    Add files via upload

---
+++ dbService.js
@@ -0,0 +1,28 @@
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+let instance = null;
+dotenv.config();
+
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT
+});
+
+
+connection.connect((err) => {
+    if(err){
+        console.log(err.message);
+
+    }
+    console.log('db'+ connection.state);
+    
+});
+
+
+module.exports = connection;
+
+
